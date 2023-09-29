ef9cd9af98b32b27101bb183b6dac0dd012a6cc4
memic84 <seadmemic@gmail.com>
2023-09-29 20:47:00 +0200

    Initial commit

---
+++ layout/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <Html>
+    <Body>
+      <div>
+        <NuxtLoadingIndicator
+          color="repeating-linear-gradient(to right,#EF4135 0%,#EF4135 50%,#ffffff 100%)"
+        />
+
+        <main>
+          <slot />
+        </main>
+      </div>
+    </Body>
+  </Html>
+</template>
